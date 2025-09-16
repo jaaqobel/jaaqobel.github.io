c37ebbad0a339e0e0bf8b722dec95c60a856514e
jaaqobel <146698602+jaaqobel@users.noreply.github.com>
2025-09-16 13:36:03 +0200

    Create zmienne.php

---
+++ zmienne.php
@@ -0,0 +1,8 @@
+<?php
+$imie = "Jan";             // zmienna typu string
+$wiek = 25;                // zmienna typu integer (liczba całkowita)
+$wysokosc = 1.82;          // zmienna typu float (liczba zmiennoprzecinkowa)
+$czyStudent = true;        // zmienna typu boolean (true lub false)
+
+echo "Cześć, nazywam się $imie i mam $wiek lat.";
+?>
